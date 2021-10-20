b1398f74f00c1a061d768665ae464f0f087f74a6
pabloporzio <pabloporzio97@gmail.com>
2021-10-20 20:08:50 -0300

    Chapter 1 - Lesson 3 - Install and use Vuex

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);
+
+new Vuex.Store({
+    state: {
+        products: [],
+    },
+    getters: {
+        productsCount() {
+
+        },
+    },
+    actions: {
+        fetchProducts() {
+
+        }
+    },
+    mutations: {
+        setProducts() {
+
+        }
+    }
+});
